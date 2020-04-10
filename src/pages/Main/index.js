e86891af159016c03f641aa08779bf0fa1c0a392
Marcelo Sousa <marcelo.sousa1997@hotmail.com>
2020-04-09 21:20:26 -0300

    Realizando navegação

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 import {Keyboard,ActivityIndicator} from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
 import Icon from 'react-native-vector-icons/MaterialIcons';
@@ -22,6 +23,16 @@ import {
 
 export default class Main extends Component {
 
+  static navigationOptions = {
+    title: 'Usuários',
+  }
+
+  static propTypes = {
+    navigation: PropTypes.shape({
+      navigate: PropTypes.func,
+    }).isRequired,
+  }
+
   state = {
     newUser: '',
     users: [],
@@ -29,6 +40,9 @@ export default class Main extends Component {
   }
 
   async componentDidMount(){
+
+    console.tron.log(this.props);
+
     const users = await AsyncStorage.getItem('users');
 
     if(users){
@@ -67,6 +81,12 @@ export default class Main extends Component {
     Keyboard.dismiss;
   }
 
+  handleNavigate = (user) =>{
+    const {navigation} = this.props;
+
+    navigation.navigate('User', {user});
+  }
+
 
   render(){
     const {users, newUser,loading} = this.state;
@@ -103,7 +123,7 @@ export default class Main extends Component {
               <Name>{item.name}</Name>
               <Bio>{item.bio}</Bio>
 
-              <ProfileButton onPress={() => {}}>
+              <ProfileButton onPress={()=> this.handleNavigate(item)}>
                 <ProfileButtonText>Ver perfil</ProfileButtonText>
               </ProfileButton>
             </User>
@@ -115,6 +135,3 @@ export default class Main extends Component {
   }
 }
 
-Main.navigationOptions = {
-  title: 'Usuários',
-}
